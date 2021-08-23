d7be65854bb67b76540ded6f1769ef65124a84e8
nick dugan <nick.duggan@workday.com>
2021-08-23 09:21:27 +0100

    Updated tiller version, changed image for argo rollout

--- demo/src/main/java/com/example/springboot/HelloController.java
+++ demo/src/main/java/com/example/springboot/HelloController.java
@@ -1,5 +1,30 @@
 package com.example.springboot;
 
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//@RestController
+//public class HelloController {
+//
+//    @RequestMapping(value = "/", method = RequestMethod.GET,
+//            produces = MediaType.IMAGE_JPEG_VALUE)
+//
+//    public void getImage(HttpServletResponse response) throws IOException {
+//
+//        var imgFile = new ClassPathResource("image/Generic_CD_Pipeline.png");
+//
+//        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+//        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+//    }
+//}
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
